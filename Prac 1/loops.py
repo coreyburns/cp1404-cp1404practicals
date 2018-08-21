9eae354d0e0d1f7d6b66ca0e53b09f447129b7ec
coreyburns <33849201+coreyburns@users.noreply.github.com>
2018-08-22 09:38:50 +1000

    could not get the end=' ' work all lines said is was a syntax error need to work more on the end input

--- Prac 1/loops.py
+++ Prac 1/loops.py
@@ -6,13 +6,26 @@ practical loops
 # first
 for i in range(1,21,2):
     print(i)
-print ("")
+print ()
 
 #a
 for i in range(0,110,10):
     print (i)
-print("")
+print()
 
 #b
 for i in range(20,0,-1):
     print(i)
+print()
+
+#c
+number_of_stars = int(input(" How Many Stars: "))
+for i in range(number_of_stars):
+    print('*')
+print()
+
+#d
+number_of_stars = int(input(" How Many Stars: "))
+for i in range(1,number_of_stars + 1):
+    print('*' * i)
+print()
