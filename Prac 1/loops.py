c8acc8e53d77d6f2daa178b0e9b582f5b3bd5d61
coreyburns <33849201+coreyburns@users.noreply.github.com>
2018-08-21 17:03:36 +1000

    written code finished and works

---
+++ Prac 1/loops.py
@@ -0,0 +1,18 @@
+'''
+cp1404
+practical loops
+'''
+
+# first
+for i in range(1,21,2):
+    print(i)
+print ("")
+
+#a
+for i in range(0,110,10):
+    print (i)
+print("")
+
+#b
+for i in range(20,0,-1):
+    print(i)
